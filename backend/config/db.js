308639138e5c2dbe049e41c5a125a722df9e701c
SUBAPARVATHI <146979395+SUBAPARVATHI@users.noreply.github.com>
2025-03-22 15:19:43 +0000

    backenddd

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log('MongoDB connected');
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("MongoDB Connected...");
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    console.error(error.message);
     process.exit(1);
   }
 };
